a0885dd5697ace4c5cae55e25d91295403444e29
Vitalii Kalinchyk <karlkory@gmail.com>
2022-07-11 16:52:27 +0300

    DecrementingCarouselWithLimitedRun

--- src/main/java/com/epam/rd/autotasks/CarouselRun.java
+++ src/main/java/com/epam/rd/autotasks/CarouselRun.java
@@ -1,13 +1,38 @@
 package com.epam.rd.autotasks;
 
 public class CarouselRun {
+    private final DecrementingCarousel carousel;
+    private int currentPosition=0;
+
+    public CarouselRun(DecrementingCarousel carousel) {
+        this.carousel=carousel;
+    }
 
     public int next() {
-       throw new UnsupportedOperationException();
+        if (isFinished())
+            return -1;
+
+        if (currentPosition >= carousel.capacity)
+            currentPosition = 0;
+
+        while (carousel.values[currentPosition]==0){
+            currentPosition++;
+            if (currentPosition >= carousel.capacity)
+                currentPosition = 0;
+        }
+        carousel.actionLimit--;
+        return carousel.values[currentPosition++]--;
     }
 
     public boolean isFinished() {
-        throw new UnsupportedOperationException();
-    }
+        if (carousel instanceof DecrementingCarouselWithLimitedRun && carousel.actionLimit==0)
+            return true;
 
+        for (int i:carousel.values) {
+            if (i>0)
+                return false;
+        }
+        return true;
+    }
 }
+
--- src/main/java/com/epam/rd/autotasks/DecrementingCarouselWithLimitedRun.java
+++ src/main/java/com/epam/rd/autotasks/DecrementingCarouselWithLimitedRun.java
@@ -3,5 +3,6 @@ package com.epam.rd.autotasks;
 public class DecrementingCarouselWithLimitedRun extends DecrementingCarousel{
     public DecrementingCarouselWithLimitedRun(final int capacity, final int actionLimit) {
         super(capacity);
+        this.actionLimit=actionLimit;
     }
 }
